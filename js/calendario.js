790be032144ed76ecdabfff37820fec6e7719e90
JosiasWeberMachado <weberjosias1@gmail.com>
2022-01-19 11:15:15 -0300

    altera o schedules.html, priemiras configurações fullcalemdar

---
+++ js/calendario.js
@@ -0,0 +1,21 @@
+(function(win,doc) {
+  
+  let calendarEl = doc.querySelector('.calendar');
+  let calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: 'dayGridMonth',
+    headerToolbar:{
+      start: 'prev,next,today',
+      center: 'title',
+      end: 'dayGridMonth, timeGridWeek, timeGridDay',
+    },
+    locale: 'pt-br',
+    buttonText:{
+      today: 'Hoje',
+      month: 'Mês',
+      week: 'Semana',
+      day: 'Dia'
+    }
+  });
+  calendar.render();
+
+})(window,document)
